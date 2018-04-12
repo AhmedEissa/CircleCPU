7259e106e5d194fc3a8389bb64a87260c4be9b79
Matthew <matbulat@gmail.com>
2018-04-12 18:38:01 +0100

    removed import

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -2,7 +2,6 @@ package sample;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.concurrent.Task;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
